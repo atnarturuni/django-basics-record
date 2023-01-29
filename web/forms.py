6bdb01964dec00ab32e00ce8d18e320e72e22614
atnartur <i@atnartur.ru>
2023-01-29 17:58:29 +0400

    + user unique validation

--- web/forms.py
+++ web/forms.py
@@ -1,10 +1,10 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class RegistrationForm(forms.Form):
-    email = forms.EmailField()
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
+class RegistrationForm(forms.ModelForm):
     password2 = forms.CharField(widget=forms.PasswordInput())
 
     def clean(self):
@@ -12,3 +12,7 @@ class RegistrationForm(forms.Form):
         if cleaned_data['password'] != cleaned_data['password2']:
             self.add_error("password", "Пароли не совпадают")
         return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "password", "password2")
