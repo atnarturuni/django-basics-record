11fda7a50bb8b50ca555a2948d73dba4aa472650
atnartur <i@atnartur.ru>
2023-01-29 17:02:59 +0400

    + render variable in template

--- web/views.py
+++ web/views.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def main_view(request):
-    return render(request, "web/main.html")
+    year = datetime.now().year
+    return render(request, "web/main.html", {
+        "year": year
+    })
