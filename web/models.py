d06e966c8cbcc9c34af6827d43d99b812ef5cda7
atnartur <i@atnartur.ru>
2023-01-29 15:38:55 +0400

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TimeSlotTag(models.Model):
+    title = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class TimeSlot(models.Model):
+    title = models.CharField(max_length=256)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_realtime = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(TimeSlotTag)
+
+
+class Holiday(models.Model):
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
