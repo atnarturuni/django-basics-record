008725a9a4e3419d74033debf8ea2f2e9adb3b3e
atnartur <i@atnartur.ru>
2023-01-29 18:24:18 +0400

    + incorrect auth check

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 from web.forms import RegistrationForm, AuthForm
 
@@ -35,4 +35,10 @@ def registration_view(request):
 
 def auth_view(request):
     form = AuthForm()
+    if request.method == 'POST':
+        form = AuthForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is None:
+                form.add_error(None, "Введены неверные данные")
     return render(request, "web/auth.html", {"form": form})
