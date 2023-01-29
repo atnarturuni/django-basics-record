3ec00ba7b97a730efee3ff7f8b9a3aebcc5d8d86
atnartur <i@atnartur.ru>
2023-01-29 17:47:47 +0400

    + form validation

--- web/forms.py
+++ web/forms.py
@@ -6,3 +6,9 @@ class RegistrationForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
     password2 = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password'] != cleaned_data['password2']:
+            self.add_error("password", "Пароли не совпадают")
+        return cleaned_data
--- web/views.py
+++ web/views.py
@@ -14,4 +14,8 @@ def main_view(request):
 
 def registration_view(request):
     form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
     return render(request, "web/registration.html", {"form": form})
