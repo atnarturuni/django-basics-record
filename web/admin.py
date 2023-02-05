7ccf1338fe1d913c5c999b67d3050088824f47e4
atnartur <i@atnartur.ru>
2023-02-05 19:10:11 +0400

    + admin parameters

--- web/admin.py
+++ web/admin.py
@@ -4,7 +4,11 @@ from web.models import TimeSlot
 
 
 class TimeSlotAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'start_date', 'end_date', 'get_spent_time')
+    list_display = ('id', 'title', "user", 'start_date', 'end_date', 'get_spent_time')
+    search_fields = ("id", "title")
+    list_filter = ("start_date", "end_date", "is_realtime")
+    ordering = ('-start_date',)
+    readonly_fields = ('is_realtime',)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate_spent_time()
