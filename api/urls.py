b2659f9f314b9309ca451e1469d6edd2e32ef4be
atnartur <i@atnartur.ru>
2023-02-06 10:16:53 +0400

    + model view set

--- api/urls.py
+++ api/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import SimpleRouter
 
-from api.views import main_view, timeslots_view
+from api.views import main_view, TimeslotModelViewSet
+
+router = SimpleRouter()
+router.register("timeslots", TimeslotModelViewSet, basename='timeslots')
 
 urlpatterns = [
     path("", main_view),
     path("token/", obtain_auth_token),
-    path("timeslots/", timeslots_view)
+    *router.urls
 ]
